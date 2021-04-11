8d39950a4d9df1d53325994a442c1ecddd3bef17
OrionXD <sajad.laskar1@gmail.com>
2021-04-11 19:11:29 +0530

    updated files and UI

--- HomePage/views.py
+++ HomePage/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'HomePage/index.html')
+    context = {'index_active': 'active'}
+    return render(request, 'Homepage/index.html', context)
+
+def about(request):
+    context = {'about_active': 'active'}
+    return render(request, 'Homepage/about.html', context)
+
+def contact(request):
+    context = {'contact_active': 'active'}
+    return render(request, 'Homepage/contact.html', context)
