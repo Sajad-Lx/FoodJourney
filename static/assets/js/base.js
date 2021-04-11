9a5b71a9c3ff868b6b23732642ba12a5b4ee93bc
OrionXD <sajad.laskar1@gmail.com>
2021-04-11 20:48:11 +0530

    Dynamic scripts and css.

--- static/assets/js/base.js
+++ static/assets/js/base.js
@@ -105,22 +105,6 @@
       });
     });
   
-    // Toggle .header-scrolled class to #header when page is scrolled
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('#header').addClass('header-scrolled');
-        $('#topbar').addClass('topbar-scrolled');
-      } else {
-        $('#header').removeClass('header-scrolled');
-        $('#topbar').removeClass('topbar-scrolled');
-      }
-    });
-  
-    if ($(window).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-      $('#topbar').addClass('topbar-scrolled');
-    }
-  
     // Real view height for mobile devices
     if (window.matchMedia("(max-width: 767px)").matches) {
       $('#hero').css({
---
+++ static/assets/js/index.js
@@ -0,0 +1,15 @@
+// Toggle .header-scrolled class to #header when page is scrolled
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+      $('#topbar').addClass('topbar-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+      $('#topbar').removeClass('topbar-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+    $('#topbar').addClass('topbar-scrolled');
+  }
